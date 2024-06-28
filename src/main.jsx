1759df4dc8d055052c37c40b1f50f39ecf54eb94
Esteban Camacho Salgado <6esteban.dev@gmail.com>
2024-06-27 20:53:11 -0500

    imports style sheets in main.jsx OK

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+
+// Importar assets (recursos: hojas de estilo, imágens, fuentes, etc.)
+import './assets/fonts/fontawesome-free-6.1.2-web/css/all.css';
+import './assets/css/normalize.css';
+import './assets/css/responsive.css';
+import './assets/css/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+
     <App />
-  </React.StrictMode>,
+
 )
