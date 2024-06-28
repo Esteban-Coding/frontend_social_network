b4a88cbdec04ead3d41a18a7184401607800cd8b
Esteban Camacho Salgado <6esteban.dev@gmail.com>
2024-06-27 21:32:15 -0500

    register OK

--- src/components/user/Register.jsx
+++ src/components/user/Register.jsx
@@ -1,9 +1,16 @@
 import { Global } from "../../helpers/Global";
 import { useForm } from "../../hooks/useForm";
+import Swal from "sweetalert2"; // Importa SweetAlert2
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export const Register = () => {
   // Usamos el hook personalizado useForm para cargar los datos del formulario
   const { form, changed } = useForm({});
+  // Estado para mostrar resultado del registro del user
+  const [saved, setSaved] = useState("not sended");
+  // Hook para redirigir
+  const navigate = useNavigate();
 
   // Guardar un usuario en la BD
   const saveUser = async (e) => {
@@ -25,11 +32,28 @@ export const Register = () => {
     // Obtener la información retornada por la request
     const data = await request.json();
 
-    console.log(data);
+    // Verificar si el estado de la respuesta del backend es "created" seteamos la variable saved con "saved" y si no, le asignamos "error", esto es para mostrar por pantalla el resultado del registro del usuario
+    if (data.status == "created") {
+      setSaved("saved");
 
-    // Redirigir a la página principal
-    if (data.status === "created") {
-      window.location.href = "/";
+      // Mostrar modal de éxito
+      Swal.fire({
+        title: "¡Usuario registrado correctamente!",
+        icon: "success",
+        confirmButtonText: "Continuar",
+      }).then(() => {
+        // Redirigir después de cerrar el modal
+        navigate("/login");
+      });
+    } else {
+      setSaved("error");
+
+      // Mostrar modal de error
+      Swal.fire({
+        title: "¡El usuario no se ha registrado!",
+        icon: "error",
+        confirmButtonText: "Intentar nuevamente",
+      });
     }
   };
 
@@ -41,35 +65,57 @@ export const Register = () => {
 
       {/* Formulario de Registro*/}
       <div className="content__posts">
-        <form className="register-form" onSubmit={saveUser}>
-          <div className="form-group">
-            <label htmlFor="name">Nombres</label>
-            <input type="text" name="name" required onChange={changed} />
-          </div>
-          <div className="form-group">
-            <label htmlFor="last_name">Apellidos</label>
-            <input type="text" name="last_name" required onChange={changed} />
-          </div>
-          <div className="form-group">
-            <label htmlFor="nick">Nick</label>
-            <input type="text" name="nick" required onChange={changed} />
-          </div>
-          <div className="form-group">
-            <label htmlFor="email">Correo Electrónico</label>
-            <input type="email" name="email" required onChange={changed} />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Contraseña</label>
-            <input
-              type="password"
-              name="password"
-              required
-              onChange={changed}
-            />
-          </div>
+        <div className="form-style">
+          {/* Respuestas de usuario registrado*/}
+          {saved == "saved" ? (
+            <strong className="alert alert-success">
+              ¡Usuario registrado correctamente!
+            </strong>
+          ) : (
+            ""
+          )}
+          {saved == "error" ? (
+            <strong className="alert alert-danger">
+              ¡El usuario no se ha registrado!
+            </strong>
+          ) : (
+            ""
+          )}
 
-          <input type="submit" value="Regístrate" className="btn btn-success" />
-        </form>
+          <form className="register-form" onSubmit={saveUser}>
+            <div className="form-group">
+              <label htmlFor="name">Nombres</label>
+              <input type="text" name="name" required onChange={changed} />
+            </div>
+            <div className="form-group">
+              <label htmlFor="last_name">Apellidos</label>
+              <input type="text" name="last_name" required onChange={changed} />
+            </div>
+            <div className="form-group">
+              <label htmlFor="nick">Nick</label>
+              <input type="text" name="nick" required onChange={changed} />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Correo Electrónico</label>
+              <input type="email" name="email" required onChange={changed} />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Contraseña</label>
+              <input
+                type="password"
+                name="password"
+                required
+                onChange={changed}
+              />
+            </div>
+
+            <input
+              type="submit"
+              value="Regístrate"
+              className="btn btn-success"
+            />
+          </form>
+        </div>
       </div>
     </>
   );
