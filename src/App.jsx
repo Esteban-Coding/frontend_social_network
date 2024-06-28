25cb60565d5adb1429be6efaef06de17878e8898
Esteban Camacho Salgado <6esteban.dev@gmail.com>
2024-06-27 21:54:27 -0500

    fix App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
+import { Routing } from "./router/Routing";
 
 function App() {
-
   return (
-    <div className='layout'>
-
+    <div className="layout">
       {/* Cargamos toda la configuración de rutas */}
       <Routing />
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
