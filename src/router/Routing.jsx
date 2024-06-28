21a90398b4c24b2b548876ecaf676e2c88f31455
Esteban Camacho Salgado <6esteban.dev@gmail.com>
2024-06-27 21:36:51 -0500

    profile get information from backend OK

--- src/router/Routing.jsx
+++ src/router/Routing.jsx
@@ -1,31 +1,34 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { PrivateLayout } from "../components/layout/private/PrivateLayout";
+import { PublicLayout } from "../../src/components/layout/public/PublicLayout";
+import { PrivateLayout } from "../../src/components/layout/private/PrivateLayout";
 import { Login } from "../components/user/Login";
 import { Register } from "../components/user/Register";
-import { PublicLayout } from "../components/layout/public/PublicLayout";
 import { Feed } from "../components/publication/Feed";
 import { Error404 } from "../components/layout/Error404";
+import { AuthProvider } from "../context/AuthProvider";
 
 export const Routing = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        {/* Cargamos los componentes de la ruta pública */}
-        <Route path="/" element={<PublicLayout />}>
-          <Route index element={<Login />} />
-          <Route path="login" element={<Login />} />
-          <Route path="registro" element={<Register />} />
-        </Route>
+      <AuthProvider>
+        <Routes>
+          {/* Cargamos los componentes de la ruta pública en rutas anidadas*/}
+          <Route path="/" element={<PublicLayout />}>
+            <Route index element={<Login />} />
+            <Route path="login" element={<Login />} />
+            <Route path="registro" element={<Register />} />
+          </Route>
 
-        {/* Cargamos los componentes de la ruta privada */}
-        <Route path="/rsocial" element={<PrivateLayout />}>
-          <Route index element={<Feed />} />
-          <Route path="feed" element={<Feed />} />
-        </Route>
+          {/* Cargamos los componentes de la ruta privada  en rutas anidadas*/}
+          <Route path="/rsocial" element={<PrivateLayout />}>
+            <Route index element={<Feed />} />
+            <Route path="feed" element={<Feed />} />
+          </Route>
 
-        {/* Configurar la ruta para el componente de Error404 */}
-        <Route path="*" element={<Error404 />} />
-      </Routes>
+          {/* Configuramos la ruta para el error 404 */}
+          <Route path="*" element={<Error404 />} />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 };
