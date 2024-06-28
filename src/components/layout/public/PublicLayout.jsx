f1e46ae65d8e6989d1272e5db7b9c29288b6e53d
Esteban Camacho Salgado <6esteban.dev@gmail.com>
2024-06-27 21:08:21 -0500

    Routes for PrivateLayout OK, Feed component OK.

---
+++ src/components/layout/public/PublicLayout.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+import { HeaderPub } from "./HeaderPub";
+
+export const PublicLayout = () => {
+  return (
+    <>
+      {/* LAYOUT */}
+      <HeaderPub />
+
+      {/* Contenido Principal */}
+      <section className="layout__content">
+        <Outlet />
+      </section>
+    </>
+  );
+};
--- src/router/Routing.jsx
+++ src/router/Routing.jsx
@@ -2,20 +2,25 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { PrivateLayout } from "../components/layout/private/PrivateLayout";
 import { Login } from "../components/user/Login";
 import { Register } from "../components/user/Register";
+import { PublicLayout } from "../components/layout/public/PublicLayout";
+import { Feed } from "../components/publication/Feed";
 
 export const Routing = () => {
   return (
     <BrowserRouter>
       <Routes>
         {/* Cargamos los componentes de la ruta pública */}
-
-        <Route path="/" element={<PrivateLayout />}>
+        <Route path="/" element={<PublicLayout />}>
           <Route index element={<Login />} />
           <Route path="login" element={<Login />} />
           <Route path="registro" element={<Register />} />
         </Route>
 
         {/* Cargamos los componentes de la ruta privada */}
+        <Route path="/rsocial" element={<PrivateLayout />}>
+          <Route index element={<Feed />} />
+          <Route path="feed" element={<Feed />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
